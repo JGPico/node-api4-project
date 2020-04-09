6f9901b12d64d702c0e311885d96d87f733c611d
JGPico <minq_2@hotmail.com>
2020-04-09 13:52:03 -0600

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const server = require('./server.js');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`\n Server running on port ${port} \n`);
